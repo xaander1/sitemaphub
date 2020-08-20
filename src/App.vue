4f48182a1f18497966068f7e7aa2f02d78865445
Alexander <alexnjogu52@gmail.com>
2020-08-20 22:58:17 +0300

    Satisfactory changes

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <Menu></Menu>
     <Intro></Intro>
+    <Form></Form>
   </div>
 </template>
 
 <script>
 import Menu from './components/Menu.vue'
 import Intro from './components/Intro.vue'
+import Form from './components/Form.vue'
 
 export default {
   name: 'App',
   components: {
     Menu,
-    Intro
+    Intro,
+    Form
   }
 }
 </script>
---
+++ src/components/Form.vue
@@ -0,0 +1,8 @@
+<template>
+	<sui-container text-align="center">
+		<div class="ui icon small input focus fluid olive">
+				<input class="prompt olive" @keyup.enter="submit" v-model="submit" type="text" placeholder="Enter Sitemap URL...">
+				<i class="inverted circular sitemap link icon olive" v-on:click="searchItem"></i>
+			</div>
+	</sui-container>
+</template>
