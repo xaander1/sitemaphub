e59132fe55e2ee72ec2991a789a2b6cf2aee6eaa
Alexander <alexnjogu52@gmail.com>
2020-08-20 22:31:35 +0300

    Working progress

---
+++ src/components/Intro.vue
@@ -0,0 +1,8 @@
+<template>
+	<sui-segment basic>
+		<h1 is="sui-header" text-align="center">
+			<sui-icon name="sitemap" color="olive" />
+			Update Your Sitemap From Keywords
+		</h1>
+	</sui-segment>
+</template>
---
+++ src/components/Menu.vue
@@ -0,0 +1,7 @@
+<template>
+	<sui-menu borderless>
+		<sui-menu-item link name="Home">
+			<sui-image :src="require('@/assets/logo.png')" size="small"/>
+		</sui-menu-item>
+	</sui-menu>
+</template>
