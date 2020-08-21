b7f080d6ac67dc9a5d5d842e75dc7704e88ba4bf
xaander1 <alexnjogu52@gmail.com>
2020-08-21 05:55:46 -0400

    Working progress

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,26 +1,39 @@
 <template>
 	<sui-container text-align="center">
 		<div class="ui icon small input focus fluid olive">
-			<input class="prompt olive" @keyup.enter="getSitemap" v-model="url" type="text" placeholder="Enter Sitemap URL...">
+			<input class="prompt olive" @keyup.enter="getSitemap" v-model="sitemapUrl" type="text" placeholder="Enter Sitemap URL...">
 			<i class="inverted circular sitemap link icon olive" v-on:click="getSitemap"></i>
 		</div>
+		{{res}}
 	</sui-container>
 </template>
 <script>
+	import axios from 'axios'
+	import cheerio from 'cheerio'
 	export default {
 		data(){
 			return {
-				url:"",
+				sitemapUrl:"https://www.chekiprice.co.ke/sitemap.xml",
 				res:""
 			}
 		},
 		methods : {
 			getSitemap(){
-				this.$request('http://www.google.com', function (error, response, body) {
-				console.error('error:', error); // Print the error if one occurred
-				console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-				console.log('body:', body); // Print the HTML for the Google homepage.
-			})
+				axios.get(this.sitemapUrl, {
+					"Content-Type": "application/xml; charset=utf-8"
+				})
+				.then(response => {
+					const $ = cheerio.load(response.data)
+					const sitemapUrls= [];
+
+					$('url loc').each(function(i) {
+						sitemapUrls[i] = $(this).text();
+					});
+					//replace
+					let newSitemapUrls = sitemapUrls.map(url=>{
+						return (url.replace('https://www.chekiprice.co.ke/','').replace('generalstores/','').replace('electronics/','').replace('allstores/','').replace('supermarkets/','').replace('appliances/','').replace('fashion/','').replace('custom/','').replace('terms-of-use','').replace('privacy-policy','').split('+').join(' '))
+					}).filter(item=>item)
+				})
 			}
 		}
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import SuiVue from 'semantic-ui-vue'
-import request from 'request'
 import 'semantic-ui-css/semantic.min.css'
 
 Vue.use(SuiVue)
-Vue.use(request)
 Vue.config.productionTip = false
 
 new Vue({
