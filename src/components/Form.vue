641e029b7981f884a4552557c500ad2dcba16c24
xaander1 <alexnjogu52@gmail.com>
2020-08-21 10:07:14 -0400

    More working progress

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -9,39 +9,55 @@
 			<sui-divider hidden />
 			<sui-form-field>
 				<label>Enter Keywords Separated By Comma's</label>
-				<textarea rows="8"></textarea>
+				<textarea v-model="userInput" rows="5"></textarea>
 			</sui-form-field>
 			<sui-header dividing>Select Category Of Sitemap</sui-header>
 			<sui-form-fields equal-width>
 				<sui-form-field>
-					<sui-checkbox toggle label="Generalstores" />
+					<sui-checkbox v-model="checkbox.generalstores" toggle label="Generalstores" />
 				</sui-form-field>
 				<sui-form-field>
-					<sui-checkbox toggle label="Electronics" />
+					<sui-checkbox v-model="checkbox.electronics" toggle label="Electronics" />
 				</sui-form-field>
 				<sui-form-field>
-					<sui-checkbox toggle label="Supermarkets" />
+					<sui-checkbox v-model="checkbox.supermarkets" toggle label="Supermarkets" />
 				</sui-form-field>
 				<sui-form-field>
-					<sui-checkbox toggle label="Fashion" />
+					<sui-checkbox v-model="checkbox.fashion" toggle label="Fashion" />
 				</sui-form-field>
 				<sui-form-field>
-					<sui-checkbox toggle label="Appliances" />
+					<sui-checkbox v-model="checkbox.appliances" toggle label="Appliances" />
 				</sui-form-field>
 			</sui-form-fields>
+			<sui-divider hidden />
+			<sui-button @click.prevent="generateSitemaps" color="olive" content="Generate Sitemap" float="right" />
 		</sui-form>	
-
-		
+		<sui-message v-if="message">
+			<pre>
+				{{message}}
+			</pre>
+		</sui-message>
 	</sui-container>
 </template>
 <script>
 	import axios from 'axios'
 	import cheerio from 'cheerio'
+	// import format from 'xml-formatter'
 	export default {
 		data(){
 			return {
 				sitemapUrl:"https://www.chekiprice.co.ke/sitemap.xml",
-				existing:null
+				existing:null,
+				checkbox:{
+					'generalstores':false,
+					'electronics':false,
+					'supermarkets':false,
+					'fashion':false,
+					'appliances':false
+				},
+				userInput:null,
+				message:'',
+				date:(new Date().getDate()+'-'+(new Date().getMonth()+1)+'-'+new Date().getFullYear())
 			}
 		},
 		methods : {
@@ -56,10 +72,33 @@
 					$('url loc').each(function(i) {
 						sitemapUrls[i] = $(this).text();
 					});
-					//replace
+					//replace and get keywords
 					this.existing = sitemapUrls.map(url=>{
 						return (url.replace('https://www.chekiprice.co.ke/','').replace('generalstores/','').replace('electronics/','').replace('allstores/','').replace('supermarkets/','').replace('appliances/','').replace('fashion/','').replace('custom/','').replace('terms-of-use','').replace('privacy-policy','').split('+').join(' '))
 					}).filter(item=>item)
+				})
+			},
+			generateSitemaps(){
+				let products = this.userInput.split(',')
+				products.forEach(item=>{
+					//check if in array
+					if(!this.existing.includes(item)){
+						let newItem=item.split(" ").join('+')
+						//loop checkboxes
+						for(let key in this.checkbox){
+							if(this.checkbox[key]){
+								let msg=`
+<url>
+	<loc>
+		https://www.chekiprice.co.ke/${key}/${newItem}
+	</loc>
+	<lastmod>${this.date}</lastmod>
+	<priority>0.5</priority>
+</url>`
+								this.message=this.message+msg
+							}
+						}
+					}
 				})
 			}
 		}
