077c3100ca8993212c9269ac809ea08599403784
xaander1 <alexnjogu52@gmail.com>
2020-08-21 10:06:54 -0400

    working progress

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,10 +1,37 @@
 <template>
-	<sui-container text-align="center">
-		<div class="ui icon small input focus fluid olive">
+	<sui-container>
+
+		<div v-if="!existing" class="ui icon small input focus fluid olive">
 			<input class="prompt olive" @keyup.enter="getSitemap" v-model="sitemapUrl" type="text" placeholder="Enter Sitemap URL...">
 			<i class="inverted circular sitemap link icon olive" v-on:click="getSitemap"></i>
 		</div>
-		{{res}}
+		<sui-form v-else>
+			<sui-divider hidden />
+			<sui-form-field>
+				<label>Enter Keywords Separated By Comma's</label>
+				<textarea rows="8"></textarea>
+			</sui-form-field>
+			<sui-header dividing>Select Category Of Sitemap</sui-header>
+			<sui-form-fields equal-width>
+				<sui-form-field>
+					<sui-checkbox toggle label="Generalstores" />
+				</sui-form-field>
+				<sui-form-field>
+					<sui-checkbox toggle label="Electronics" />
+				</sui-form-field>
+				<sui-form-field>
+					<sui-checkbox toggle label="Supermarkets" />
+				</sui-form-field>
+				<sui-form-field>
+					<sui-checkbox toggle label="Fashion" />
+				</sui-form-field>
+				<sui-form-field>
+					<sui-checkbox toggle label="Appliances" />
+				</sui-form-field>
+			</sui-form-fields>
+		</sui-form>	
+
+		
 	</sui-container>
 </template>
 <script>
@@ -14,7 +41,7 @@
 		data(){
 			return {
 				sitemapUrl:"https://www.chekiprice.co.ke/sitemap.xml",
-				res:""
+				existing:null
 			}
 		},
 		methods : {
@@ -30,7 +57,7 @@
 						sitemapUrls[i] = $(this).text();
 					});
 					//replace
-					let newSitemapUrls = sitemapUrls.map(url=>{
+					this.existing = sitemapUrls.map(url=>{
 						return (url.replace('https://www.chekiprice.co.ke/','').replace('generalstores/','').replace('electronics/','').replace('allstores/','').replace('supermarkets/','').replace('appliances/','').replace('fashion/','').replace('custom/','').replace('terms-of-use','').replace('privacy-policy','').split('+').join(' '))
 					}).filter(item=>item)
 				})
