9f0c28cd663f131e21a3ea873f1a9b7a9dac2a5a
Alexander <alexnjogu52@gmail.com>
2020-08-22 22:58:11 +0300

    deploy github

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,11 +1,11 @@
 <template>
 	<sui-menu borderless>
-		<a href="/" is="sui-menu-item" link name="Home">
+		<a href="https://xaander1.github.io/sitemaphub/" is="sui-menu-item" link name="Home">
 			<sui-image :src="require('@/assets/logo.png')" size="small"/>
 		</a>
 		<sui-menu-menu position="right">
-			<a href="https://gitlab.com/xaander1/sitemaphub" target="_blank" is="sui-menu-item" link name="Home">
-				<sui-icon color="orange" name="gitlab"/> View Project
+			<a href="https://github.com/xaander1/sitemaphub" target="_blank" is="sui-menu-item" link name="Home">
+				<sui-icon color="green" size="large" name="github"/> View Project
 			</a>
 		</sui-menu-menu>
 	</sui-menu>
